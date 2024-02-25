603611ec36da17d48c013ede60f8963f8a75c04c
Nicklas Boman <smurfd@gmail.com>
2024-02-26 00:49:33 +0100

    no flake ignore

--- bitrs/test/test_bitrs.py
+++ bitrs/test/test_bitrs.py
@@ -7,14 +7,16 @@ import time
 # dummy function
 def logr(x, y) -> Any:
   ret = []
-  for _ in range(100000): ret = x * y
+  for _ in range(100000):
+    ret = x * y
   return ret
 
 
 def test_bitrs() -> None:
   t = time.perf_counter()
   b = Bitrs(logr, 1337, 7331)
-  for _ in range(10): b.start()
+  for _ in range(10):
+    b.start()
   print(b.stop())
   print("time {:.4f}".format(time.perf_counter() - t))
 
